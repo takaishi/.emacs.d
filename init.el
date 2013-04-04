22196c9b15c2734760c210eb0488415f16deff63
Ryo TAKAISHI <ryo.takaishi.0@gmail.com>
2013-04-04 13:01:12 +0900

    bundleを使うよう変更

--- init.el
+++ init.el
@@ -1,4 +1,17 @@
-(require 'cl)
+;;(require 'cl)
+
+;; el-get
+(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
+(setq-default el-get-dir (locate-user-emacs-file "el-get")
+              el-get-emacswiki-base-url
+              "http://raw.github.com/emacsmirror/emacswiki.org/master/")
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "http://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (let (el-get-master-branch)
+      (goto-char (point-max))
+      (eval-print-last-sexp))))
 
 ;; Load up Org Mode and (now included) Org Babel for elisp embedded in Org Mode files
 (defconst dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
@@ -14,10 +27,15 @@
                                             ".." org-dir)))))
   (setq load-path (append (list org-dir org-contrib-dir)
                           (or load-path nil))))
-
-;; load up Org-mode and Org-babel
 (require 'org)
-(require 'ob-tangle)
 
-;; load up all literate org-mode files in this directory
-(mapc #'org-babel-load-file (directory-files config-dir t "\\.org$"))
+;; bunele(el-get wrapper)
+(add-to-list 'el-get-sources '(:name bundle :type github :pkgname "tarao/bundle-el"))
+(el-get 'sync 'bundle)
+
+(bundle emacs-jp/init-loader)
+(bundle takaishi/babel-loader.el)
+(require 'org-element)
+(require 'babel-loader)
+(setq init-loader-byte-compile t)
+(bl:load-dir "~/.emacs.d/conf.d/")  
